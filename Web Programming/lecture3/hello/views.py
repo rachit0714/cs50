9ca3ecd1a03406276617b06ddeb95892308482ca
rachit0714 <69870030+rachit0714@users.noreply.github.com>
2023-02-27 17:17:37 -0500

    Adding pong, fifty-bird, breakout and some sql files

--- Web Programming/lecture3/hello/views.py
+++ Web Programming/lecture3/hello/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "hello/index.html")
+
+# takes a request and name then returns a html file given the name
+def greet(request, name):
+    # a dictionary is passed that has keys of variable names and values of the value of said variable
+    return render(request, "hello/greet.html", {"name": name.capitalize()})
